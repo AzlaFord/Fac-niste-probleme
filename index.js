55442dab1a2b4b6abb7684b260e4e3646fd143bf
FORD <vbivol394@gmail.com>
2025-08-28 13:32:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+function countNegNum(arr){
+    let arrNeg = [0,0]
+    let arrPos = [0,0]
+
+    for(let i = 0 ; i <arr.length;i++){
+        if(arr[i] >= 0){
+            arrNeg[0] += arr[i]
+            arrNeg[1]+=1
+        }
+        if(arr[i] <= 0){
+            arrPos[0] += arr[i]
+            arrPos[1]++
+        }
+    }
+    return [arrNeg,arrPos]
+}
+console.log(countNegNum([1,-2,4,-6,7,-1,3]))
+
+function reverse(arr){
+    let k =0
+    for(let i = arr.length-1;k<=i;i--){
+        [arr[i], arr[k]] = [arr[k], arr[i]];
+        k++
+    }
+    return arr
+}
+
+console.log(reverse([1,2,3,4,5,6,7]))
+
+function thirdMost(arr){
+    let max3 = [-Infinity,-Infinity,-Infinity]
+
+    for(let i = 0 ; i<arr.length;i++){
+        if(max3[0] < arr[i]){
+            max3[2] = max3[1]
+            max3[1] = max3[0]
+            max3[0] = arr[i]
+        }else if(max3[1] < arr[i] && arr[i] < max3[0]){
+            max3[2] = max3[1]
+            max3[1] = arr[i]
+        }else if(max3[2]<arr[i] && arr[i] < max3[1]){
+            max3[2] = arr[i]
+        }
+    }
+    return max3
+}
+console.log(thirdMost([3,4,5,8,9,1,2,7,10,0]))
+
+function thirdMinOdd(arr){
+    min = [Infinity,Infinity,Infinity]
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] % 2 != 0){
+            if(min[0] > arr[i]){
+                min[2] = min[1]
+                min[1] = min[0]
+                min[0] = arr[i]
+            }else if(min[1] > arr[i] && arr[i] > min[0]){
+                min[2] = min[1]
+                min[1] = arr[i]
+            }else if(min[2] > arr[i] && arr[i] > min[1] ){
+                min[2] = arr[i]
+            }
+        }
+    }
+    return min
+}
+console.log(thirdMinOdd([3,5,1,8,9,2,7,10,11,0]))
---
+++ nr1.py
@@ -0,0 +1,68 @@
+arr2 = [111,23,4,5,1,2,31]
+min = arr2[0]
+max = arr2[0]
+for i in range(2,len(arr2)):
+    if max < arr2[i]:
+        max = arr2[i]
+    if min > arr2[i]:
+        min = arr2[i]
+print(min,max)
+
+def getAvgSum(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += arr[i]
+    return [sum,sum/len(arr)]
+print(getAvgSum([1,2]))
+
+def countNr(arr,nr):
+    arr2 = []
+    for i in range(len(arr)):
+        if arr[i] == nr:
+            arr2.append(i+1)
+    return arr2
+
+print(countNr([1,2,3,2,5,3],3))
+
+def countNegPos(arr):
+    countNeg = 0
+    countPos = 0
+    sumNeg = 0
+    sumPos = 0
+    for i in range(len(arr)):
+        if arr[i] >= 0:
+            sumPos += arr[i]
+            countPos+=1
+        if arr[i] <=0:
+            sumNeg += arr[i]
+            countNeg+=1
+    return [countNeg,sumNeg,countPos,sumPos]
+
+print(countNegPos([1,-2,4,-6,7,-1,3]))
+
+
+
+def reverse(arr):
+    return arr[::-1]
+print(reverse([1,2,3,4,5,6,7]))
+
+
+def thirdMax(arr):
+    arr2 = []
+    arr2.append(arr[0])
+    arr2.append(arr[0])
+    arr2.append(arr[0])
+
+    for i in range(len(arr)):
+        if(arr2[0] < arr[i]):
+            arr2[2] = arr2[1]
+            arr2[1] = arr2[0]
+            arr2[0] = arr[i]
+        elif arr2[1] < arr2[0] and arr2[1] < arr[i]:
+            arr2[2] = arr2[1]
+            arr2[1] = arr[i]
+        elif arr2[2] < arr2[1] and arr2[2] < arr[i]:
+            arr2[2] = arr[i]
+    return arr2    
+
+print(thirdMax([3,4,5,8,9,1,2,7,10,0]))
